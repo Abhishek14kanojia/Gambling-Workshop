e4c740126d4844c03e984b0927c537f5195586e8
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-10-29 23:30:23 +0530

    Gambling

---
+++ Gambling.java
@@ -0,0 +1,17 @@
+package com.workShopEX;
+import java.util.*;
+public class Gambling {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the amount of stack");
+	int Stack = sc.nextInt();
+	System.out.println("enter the price of game");
+	int betPrice = sc.nextInt();
+	
+	System.out.println(Stack);
+	System.out.println(betPrice);
+	
+	}
+
+}
